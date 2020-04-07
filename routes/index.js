9ec28aab80d91b9bd3aadf6d074003769f679078
Kelso <graemebrooks@gmail.com>
2020-04-07 18:25:15 -0500

    set up initial routes and controllers for statsheet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const allTimeStatsCtrl = require('../controllers/allTimeStats');
+
+router.get('/index/winsAndLosses', allTimeStatsCtrl.winsAndLosses);
 
 module.exports = router;
--- services/statsheet/getAllTimeWinsAndLosses.js
+++ services/statsheet/getAllTimeWinsAndLosses.js
@@ -1,34 +1,36 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { promisify } = require('util');
 
-const creds = require('./client_secret.json');
+const creds = require('../../client_secret.json');
 
-async function accessSpreadsheet() {
+async function getAllTimeWinsAndLosses() {
 	const doc = new GoogleSpreadsheet('10hTrKAubzc-uZ_RtKGVvU3cg9hhN2cDYll5p1ltfwmk');
 	await doc.useServiceAccountAuth({
 		client_email: creds.client_email,
 		private_key: creds.private_key
 	});
 	await doc.loadInfo();
-	console.log(doc.title);
 
-	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+	const sheet = doc.sheetsByIndex[0]; // all time regular season team stats
 
 	await sheet.loadCells('A1:AG13');
 
-	const a1 = sheet.getCell(0, 0);
-
+	// Load Teams
 	const teams = {};
-
 	for (let i = 1; i < 12; i++) {
-		console.log(`getting cell at (${i}, 1)`);
 		teams[i] = sheet.getCell(i, 1).value;
-		console.log(teams[i].value);
 	}
 
-	console.log(sheet.title);
+	// Generate Wins and Losses Data
+	const data = {};
+	for (let i = 1; i < 12; i++) {
+		data[teams[i]] = {
+			allTimeWins: sheet.getCell(i, 2).value,
+			allTimeLosses: sheet.getCell(i, 3).value
+		};
+	}
 
-	console.log(teams);
+	return data;
 }
 
-accessSpreadsheet();
+module.exports = getAllTimeWinsAndLosses;
