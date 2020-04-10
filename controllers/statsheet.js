20241cdeea99187130c8a30d56d42ad1c3d71fc8
Kelso <graemebrooks@gmail.com>
2020-04-10 17:47:33 -0500

    adds CORS header

--- controllers/statsheet.js
+++ controllers/statsheet.js
@@ -7,7 +7,7 @@ module.exports = {
 async function index(req, res) {
 	result = await getStatsheet();
 	try {
-		res.status(200).json(result);
+		res.setHeader('Access-Control-Allow-Origin', '*').status(200).json(result);
 	} catch (err) {
 		res.status(400).json(err);
 	}
