d8bf43301291fead97ae80dc89d53a723d41c41d
Kelso <graemebrooks@gmail.com>
2020-04-18 19:39:16 -0500

    change port for aws

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // app.listen(port, function() {
 // 	console.log(`Express app running on port ${port}`);
