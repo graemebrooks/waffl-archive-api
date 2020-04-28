bb12ddd1cd2dec0199f2509dbadf17afef7589d7
Kelso <graemebrooks@gmail.com>
2020-04-28 09:53:48 -0500

    adds 2017 draft data

--- controllers/draftData.js
+++ controllers/draftData.js
@@ -2,7 +2,9 @@ const get2014DraftData = require('../services/draftData/get2014DraftData');
 
 module.exports = {
 	get2014,
-	get2015
+	get2015,
+	get2016,
+	get2017
 };
 
 async function get2014(req, res) {
@@ -17,6 +19,25 @@ async function get2014(req, res) {
 
 async function get2015(req, res) {
 	result = await get2015DraftData();
+	try {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+}
+
+async function get2016(req, res) {
+	result = await get2016DraftData();
+	try {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+}
+async function get2017(req, res) {
+	result = await get2017DraftData();
 	try {
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.status(200).json(result);
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.get('/index/playerRecords', recordbookCtrl.playerRecords);
 
 router.get('/index/draftData/2014', draftDataCtrl.get2014);
 router.get('/index/draftData/2015', draftDataCtrl.get2015);
+router.get('/index/draftData/2016', draftDataCtrl.get2016);
+router.get('/index/draftData/2017', draftDataCtrl.get2017);
 
 router.get('/index/logos', miscCtrl.logos);
 router.get('/index/colors', miscCtrl.colors);
