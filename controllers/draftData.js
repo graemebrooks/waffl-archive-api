5ba631daa0d5a270a689a82a42bf4b676bb7d990
Kelso <graemebrooks@gmail.com>
2020-05-07 15:03:04 -0500

    adds draft data for 2018 & 2019

--- controllers/draftData.js
+++ controllers/draftData.js
@@ -2,12 +2,16 @@ const get2014DraftData = require('../services/draftData/get2014DraftData');
 const get2015DraftData = require('../services/draftData/get2015DraftData');
 const get2016DraftData = require('../services/draftData/get2016DraftData');
 const get2017DraftData = require('../services/draftData/get2017DraftData');
+const get2018DraftData = require('../services/draftData/get2018DraftData');
+const get2019DraftData = require('../services/draftData/get2019DraftData');
 
 module.exports = {
 	get2014,
 	get2015,
 	get2016,
-	get2017
+	get2017,
+	get2018,
+	get2019
 };
 
 async function get2014(req, res) {
@@ -39,8 +43,29 @@ async function get2016(req, res) {
 		res.status(400).json(err);
 	}
 }
+
 async function get2017(req, res) {
 	result = await get2017DraftData();
+	try {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+}
+
+async function get2018(req, res) {
+	result = await get2018DraftData();
+	try {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+}
+
+async function get2019(req, res) {
+	result = await get2019DraftData();
 	try {
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.status(200).json(result);
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ router.get('/index/draftData/2014', draftDataCtrl.get2014);
 router.get('/index/draftData/2015', draftDataCtrl.get2015);
 router.get('/index/draftData/2016', draftDataCtrl.get2016);
 router.get('/index/draftData/2017', draftDataCtrl.get2017);
+router.get('/index/draftData/2018', draftDataCtrl.get2018);
+router.get('/index/draftData/2019', draftDataCtrl.get2019);
 
 router.get('/index/logos', miscCtrl.logos);
 router.get('/index/colors', miscCtrl.colors);
